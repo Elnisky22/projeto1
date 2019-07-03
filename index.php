bb4794c0df8118356f8cbfd92f6e12a893542a1f
Leonardo Elnisky <leonardoelnisky@hotmail.com>
2019-07-03 12:12:36 -0300

    index updated

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
-<?php
+<!doctype html>
 
-//
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Projeto 1</title>
+        <meta name="description" content="Projeto 1">
+        <meta name="author" content="Leonardo Elnisky">
+    </head>
+
+    <body>
+        <form method="get" action="">
+            Nome: <input type="text"/>
+        </form>
+    </body>
+</html>
